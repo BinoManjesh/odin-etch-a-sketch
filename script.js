5cded99d4445872c7d27bb4ab5b6118aa0ebb46f
BinoManjesh <binomanjesh@gmail.com>
2022-06-23 15:39:22 +0530

    Make button change pixels per side

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-let gridSize = 16;
 let grid = document.createElement("div");
-grid.classList.add("grid");
-for (let i = 1; i <= gridSize; ++i) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 1; j <= gridSize; ++j) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        row.appendChild(cell);
+const body = document.querySelector("body");
+body.appendChild(grid);
+changeGridSize(16);
+
+function onClick() {
+    const gridSize = +prompt("Enter number of pixels per row: ")
+    if (Number.isInteger(gridSize) && 0 < gridSize && gridSize <= 100) {
+        changeGridSize(gridSize);
     }
-    grid.appendChild(row);
 }
-document.querySelector("body").appendChild(grid);
+
+function changeGridSize(gridSize) {
+    body.removeChild(grid);
+    grid = document.createElement("div");
+    grid.classList.add("grid");
+    for (let i = 1; i <= gridSize; ++i) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 1; j <= gridSize; ++j) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+    body.appendChild(grid);
+}
